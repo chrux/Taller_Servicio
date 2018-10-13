8505a92527c429e38b44d05ac401b1b9c297fe74
Christian Torres <chtorrez@gmail.com>
2018-10-13 13:52:43 -0600

    Agregando las primeras rutas de manufacturadores.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use taller_servicio\Manufacturer;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,34 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('manufacturers', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Manufacturer::all();
+});
+ 
+Route::get('manufacturers/{id}', function($id) {
+    return Manufacturer::find($id);
+});
+
+Route::post('manufacturers', function(Request $request) {
+    return Manufacturer::create($request->all());
+});
+
+Route::put('manufacturers/{id}', function(Request $request, $id) {
+    $manufacturer = Manufacturer::findOrFail($id);
+    $manufacturer->update($request->all());
+
+    return $manufacturer;
+});
+
+Route::delete('manufacturers/{id}', function($id) {
+    $manufacturer = Manufacturer::find($id);
+    if ($manufacturer) {
+        $manufacturer->delete();
+    }
+
+    return response(null, 204);
 });
